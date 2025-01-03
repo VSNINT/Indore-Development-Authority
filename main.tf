fbf356565a35d05da211589aa7824fe422952e1d
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-01-03 15:48:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+
+  # Set the environment variables for Azure
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
 }
 
 # Resource Group
