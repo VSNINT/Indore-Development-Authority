8de78808d47a6885c6635b6a3268d2e5863ff59f
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-01-03 16:35:28 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,83 +1,80 @@
 variable "location" {
-  type = string
+  type    = string
   default = "Central India"
 }
 
 variable "resource_group_name" {
-  type = string
+  type    = string
   default = "IDA_Indore_RG123"
 }
 
 variable "vnet_name" {
-  type = string
+  type    = string
   default = "IDA-Vnet1"
 }
 
 variable "vnet_address_space" {
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
+  type    = list(string)
+  default = ["10.0.0.0/16"]
 }
 
-
 variable "subnet_name" {
-  type = string
+  type    = string
   default = "IDA-AppSubnet"
 }
 
 variable "subnet_address_prefix" {
-  type = string
+  type    = string
   default = "10.0.1.0/24"
 }
 
 variable "nsg_name" {
-  type = string
+  type    = string
   default = "ida-prod-NSG"
 }
 
 variable "public_ip_name" {
-  type = string
+  type    = string
   default = "app-vm-pip"
 }
 
 variable "nic_name" {
-  type = string
+  type    = string
   default = "app-vm-NIC"
 }
 
 variable "ip_configuration_name" {
-  type = string
+  type    = string
   default = "LandingZone-IPConfig"
 }
 
-
-
 variable "sql_server_name" {
-  type = string
+  type    = string
   default = "ida-sql-server"
 }
 
 variable "sql_admin_user" {
-  type = string
+  type    = string
   default = "sqladmin"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type    = string
   default = "P@ssw0rdSQL123!"
 }
 
 variable "sql_database_name" {
-  type = string
+  type    = string
   default = "ida-prod-DB"
 }
 
 variable "app_service_plan_name" {
-  type = string
+  type    = string
   default = "ida-AppServicePlan"
 }
 
 variable "app_service_name" {
-  type = string
+  type    = string
   default = "IDAAccount-AppService"
 }
 
