8c2c01d7c271c235b6f90874ac456b32f13dca33
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-01-06 11:09:51 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,16 @@
+# Location
 variable "location" {
   type    = string
   default = "Central India"
 }
 
+# Resource Group
 variable "resource_group_name" {
   type    = string
   default = "IDA_Indore_RG123"
 }
 
+# Virtual Network
 variable "vnet_name" {
   type    = string
   default = "IDA-Vnet1"
@@ -18,6 +21,7 @@ variable "vnet_address_space" {
   default = ["10.0.0.0/16"]
 }
 
+# Subnet
 variable "subnet_name" {
   type    = string
   default = "IDA-AppSubnet"
@@ -28,16 +32,19 @@ variable "subnet_address_prefix" {
   default = "10.0.1.0/24"
 }
 
+# Network Security Group
 variable "nsg_name" {
   type    = string
   default = "ida-prod-NSG"
 }
 
+# Public IP
 variable "public_ip_name" {
   type    = string
   default = "app-vm-pip"
 }
 
+# Network Interface
 variable "nic_name" {
   type    = string
   default = "app-vm-NIC"
@@ -48,6 +55,7 @@ variable "ip_configuration_name" {
   default = "LandingZone-IPConfig"
 }
 
+# SQL Server
 variable "sql_server_name" {
   type    = string
   default = "ida-sql-server"
@@ -63,39 +71,41 @@ variable "sql_admin_password" {
   default = "P@ssw0rdSQL123!"
 }
 
+# SQL Database
 variable "sql_database_name" {
   type    = string
   default = "ida-prod-DB"
 }
 
+# App Service Plan
 variable "app_service_plan_name" {
   type    = string
   default = "ida-AppServicePlan"
 }
 
+# App Service
 variable "app_service_name" {
   type    = string
   default = "IDAAccount-AppService"
 }
-variable "ARM_CLIENT_ID" {
+
+# Azure Authentication Variables
+variable "client_id" {
   description = "The client ID of the Azure service principal"
   type        = string
 }
 
-variable "ARM_CLIENT_SECRET" {
+variable "client_secret" {
   description = "The client secret of the Azure service principal"
   type        = string
 }
 
-variable "ARM_TENANT_ID" {
+variable "tenant_id" {
   description = "The tenant ID of the Azure subscription"
   type        = string
 }
 
-variable "ARM_SUBSCRIPTION_ID" {
+variable "subscription_id" {
   description = "The subscription ID of the Azure subscription"
   type        = string
 }
-
-
-
