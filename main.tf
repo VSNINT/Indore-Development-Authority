70b630187eea013b91563f24d0c2eeb304dd0025
Ankitpatel1122 <110924535+Ankitpatel1122@users.noreply.github.com>
2025-01-06 12:39:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,7 +87,4 @@ resource "azurerm_linux_web_app" "app_service" {
   site_config {}
 }
 
-# Output the default site URL
-output "app_service_default_hostname" {
-  value = azurerm_linux_web_app.app_service.default_site_url
-}
+
